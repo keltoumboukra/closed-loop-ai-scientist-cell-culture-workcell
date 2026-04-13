43d77b9698ec7ec5f683f6a46c968e7c8da51a6e
Keltoum Boukra <keltoum.boukra96@gmail.com>
2026-04-12 18:03:38 -0700

    feat(design): add SOURCE_PLATE_ID and MAX_SOURCE_WELL_VOLUME_UL constants

--- src/design/__init__.py
+++ src/design/__init__.py
@@ -1,6 +1,11 @@
 """Experiment design generation (reagent LHS, Monomer transfer_array, validation)."""
 
-from src.design.constants import MAX_WELL_VOLUME_UL, MIN_TRANSFER_VOLUME_UL
+from src.design.constants import (
+    MAX_SOURCE_WELL_VOLUME_UL,
+    MAX_WELL_VOLUME_UL,
+    MIN_TRANSFER_VOLUME_UL,
+    SOURCE_PLATE_ID,
+)
 from src.design.lhs_plots import (
     lhs_sample_matrix_from_mapping,
     write_lhs_visualization_files,
@@ -21,8 +26,10 @@ from src.design.transfer_validation import (
 )
 
 __all__ = [
+    "MAX_SOURCE_WELL_VOLUME_UL",
     "MAX_WELL_VOLUME_UL",
     "MIN_TRANSFER_VOLUME_UL",
+    "SOURCE_PLATE_ID",
     "MonomerCellCultureTransferRow",
     "MonomerReagentTransferRow",
     "MonomerTransferRow",
--- src/design/constants.py
+++ src/design/constants.py
@@ -5,3 +5,7 @@ MAX_WELL_VOLUME_UL = 200.0
 
 # Liquid handler: do not schedule transfers below this volume (µL).
 MIN_TRANSFER_VOLUME_UL = 10.0
+
+# Single 24-well plate that holds all stocks and inoculum; max fill per well.
+SOURCE_PLATE_ID = "source_24w"
+MAX_SOURCE_WELL_VOLUME_UL = 8_500.0
